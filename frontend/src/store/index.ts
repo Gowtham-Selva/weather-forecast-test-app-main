220da7fa840109e15e99aa54a152fea3fa13f63f
Gowtham Selvaraju <gowtham.selvaraju@accionlabs.com>
2024-10-27 23:07:35 +0530

    Added Store changes

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,13 +1,29 @@
 import { createStore } from 'vuex'
 
+export interface ILocation {
+  lat: number;
+  lng: number;
+}
+
+export interface RootState {
+  selectedPlace: ILocation
+}
+
 export default createStore({
   state: {
+    selectedPlace: null as ILocation | null,
   },
   getters: {
   },
   mutations: {
+    setSelectedPlace(state, place: ILocation) {
+      state.selectedPlace = place;
+    },
   },
   actions: {
+    updateSelectedPlace({ commit }, place: ILocation) {
+      commit('setSelectedPlace', place);
+    },
   },
   modules: {
   }
